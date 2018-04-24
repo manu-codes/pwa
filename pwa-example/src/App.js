15b003a30653d03e505f9f89ef3db2e5aac1d67b
Manu Krishnan <manu.krishnan@tarams.com>
2018-04-24 18:19:26 +0530

    initial page changes and router changes

--- pwa-example/src/App.js
+++ pwa-example/src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Router, Route } from 'react-router';
+import { Link,BrowserRouter } from 'react-router-dom';
 class App extends Component {
   render() {
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Welcome to React</h1>
+    //     </header>
+    //     <p className="App-intro">
+    //       To get started, edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //   </div>
+    // );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact={true} component={Feed} />
+          <Route path='/profile' component={Profile} />
+        </div>
+        </BrowserRouter>
+    )
   }
 }
+
+
+const NavBar = () => (
+  <div className="navbar">
+    <Link className='listItem' to="/"><a>Feed</a></Link>
+    <Link className='listItem' to="/profile"><a>Profile</a></Link>
+  </div>
+);
+
+const Template = ({ title }) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      This is the {title} page.
+    </p>
+  </div>
+);
+
+const Feed = (props) => (
+  <Template title="Feed" />
+);
+const Profile = (props) => (
+  <Template title="Profile" />
+);
 
 export default App;
